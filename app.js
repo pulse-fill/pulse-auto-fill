2cfdf8d6a5e2d64c61313566db5c14d4daebbea0
pavanmajji03 <pavanmajji03@gmail.com>
2019-02-03 19:25:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ function collectRequestData(request, callback) {
 }
 
 function registerCron(msg, mail, name) {
-  cron.schedule("0 0 9 * * MON-FRI", function() {
+  cron.schedule("0 0 9 * * *", function() {
     var startDate = getTodaysDate();
     console.log(startDate);
     checkIfFilledStatusForDate(startDate,name,mail)
