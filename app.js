833323fd6d17f21a362a25497b044d41290c2af9
pavan.sai <pavan.sai@knowledgelens.com>
2019-02-03 12:28:56 +0530

    #updates

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
 const http = require("http");
-const path = require('path');
-const request = require('request'); 
+const path = require("path");
+const request = require("request");
 const cron = require("node-cron");
-
-
+const express = require("express");
 const { parse } = require("querystring");
+const app = express();
+
+app.get("/", function(req, res) {
+  // res.sendFile(path.join(__dirname, "index.html"));
+});
 
 const server = http.createServer((req, res) => {
   if (req.method === "POST") {
     collectRequestData(req, result => {
-      console.log(result);
-      registerCron(result.default_msg,result.username_mail,result.username);
-      postData(result.default_msg,result.username_mail,result.username);
-      res.end(`Successfully registered cron job to fill pulse for ${result.username_mail}`);
+      registerCron(result.default_msg, result.username_mail, result.username);
+      res.end(
+        `Successfully registered cron job to fill pulse for ${
+          result.username_mail
+        }`
+      );
     });
   } else {
     res.end(`
-            <!doctype html>
-            <html>
-            <body>
-                <form action="/" method="post">
-                    <input type="text" name="username_mail" placeholder="mail id leaving the @knowledgelens.com ." /><br />
-                    <input type="text" name="default_msg" placeholder="default message to be filled" /><br />
-                    <input type="text" name="username"  placeholder="exact username" /><br />
-                    <button>Save</button>
-                </form>
-            </body>
-            </html>
-        `);
+          <!doctype html>
+          <html>
+          <body>
+              <form action="/" method="post">
+                  <input type="text" name="username_mail" placeholder="mail id leaving the @knowledgelens.com ." /><br />
+                  <input type="text" name="default_msg" placeholder="default message to be filled" /><br />
+                  <input type="text" name="username"  placeholder="exact username" /><br />
+                  <button>Save</button>
+              </form>
+          </body>
+          </html>
+      `);
   }
 });
 server.listen(5000);
-
 function collectRequestData(request, callback) {
   const FORM_URLENCODED = "application/x-www-form-urlencoded";
   if (request.headers["content-type"] === FORM_URLENCODED) {
@@ -47,39 +52,41 @@ function collectRequestData(request, callback) {
   }
 }
 
-function registerCron(msg,mail,name) {
+function registerCron(msg, mail, name) {
   cron.schedule("* * * * *", function() {
-    postData(msg,mail,name);
-    console.log("filling pulse every minute for-- ",mail);
+    postData(msg, mail, name);
+    console.log("filling pulse every minute for-- ", mail);
   });
 }
 
-function postData(msg = '.',mail='',name=''){
-  request.post({
-    "headers": { "content-type": "application/json" },
-    "url": "https://klpulse.knowledgelens.com/status/createStatus",
-    "body": JSON.stringify({
-      "startDate": "02/01/2019",
-      "name": `${name}`,
-      "email": `${mail}@knowledgelens.com`,
-      "taskToday": `${msg}`,
-      "taskResolved": "",
-      "message": [],
-      "taskCompletion": "true",
-      "delayedStatus": "false",
-      "rating": "0",
-      "reviews": "0",
-      "ratedBy": [],
-      "goalTomorrow": "",
-      "innovationOrIdea": "",
-      "appreciation": "",
-      "pendingObstucles": ""
-    })
-}, (error, response, body) => {
-    if(error) {
+function postData(msg = ".", mail = "", name = "") {
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: "http://klpulse.knowledgelens.com/status/createStatus",
+      body: JSON.stringify({
+        startDate: "02/01/2019",
+        name: `${name}`,
+        email: `${mail}@knowledgelens.com`,
+        taskToday: `${msg}`,
+        taskResolved: "",
+        message: [],
+        taskCompletion: "true",
+        delayedStatus: "false",
+        rating: "0",
+        reviews: "0",
+        ratedBy: [],
+        goalTomorrow: "",
+        innovationOrIdea: "",
+        appreciation: "",
+        pendingObstucles: ""
+      })
+    },
+    (error, response, body) => {
+      if (error) {
         return console.dir(error);
+      }
+      console.dir(JSON.parse(body));
     }
-    console.dir(JSON.parse(body));
-});
-  
+  );
 }
